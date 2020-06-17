3fff0bd78c4c9cfbf43ea55735ff694d65087603
Béla Ormos <ormos.bela@gmail.com>
2020-06-17 17:56:29 +0200

    Refactor code

--- Speller.py
+++ Speller.py
@@ -49,26 +49,27 @@ def next_word(test_id, word, answer):
             test_words.append(word) 
     if len(test_words) == 0:
         os.remove(test_id) 
-        result_file_name = store_result()
-        result = load_file(result_file_name)
-        result.pop(0)
-        if session['type'] == 'normal':
-            return json.dumps({"result": "{} out of {}".format(session['points'], session['count']), "previous": result})
-        else:
-            return json.dumps({"result": "{} times failed".format(session['faults']), "previous": result})
+        results = store_result()
+        results.pop(0)
+        return json.dumps({"result": "{} out of {}".format(session['points'], session['count'])
+            if session['type'] == 'normal' else
+            "{} times failed".format(session['faults']), "previous": results})
     return get_next_word(test_words, test_id)
     
 
 def get_next_word(test_words, test_id):
     word = test_words[0]
     test_words.pop(0)
-    with open(test_id, 'w', encoding="utf-8") as f:
-        json.dump(test_words, f, ensure_ascii=False)
+    write_file(test_id, test_words)
     return json.dumps({"word": word, "id": test_id}, ensure_ascii=False)
     
 def load_file(file_name):
     with open(file_name, 'r', encoding="utf-8") as f:
         return json.load(f)
+        
+def write_file(file_name, content):
+    with open(file_name, 'w', encoding="utf-8") as f:
+        json.dump(content, f, ensure_ascii=False)
         
 def check_answer(word, answer):
     words = load_file(WORD_DICTIONARY)[answer]
@@ -87,10 +88,8 @@ def store_result():
         results = load_file(result_file_name)
     except:
         results = []
-    if session['type'] == 'normal':
-        results.insert(0, "{} out of {}".format(session['points'], session['count']))
-    else:
-        results.insert(0, "{} times failed".format(session['faults']))
-    with open(result_file_name, 'w', encoding="utf-8") as f:
-        json.dump(results, f, ensure_ascii=False)
-    return result_file_name
+    results.insert(0, "{} out of {}".format(session['points'], session['count'])
+        if session['type'] == 'normal' else
+        "{} times failed".format(session['faults']))
+    write_file(result_file_name, results)
+    return results
